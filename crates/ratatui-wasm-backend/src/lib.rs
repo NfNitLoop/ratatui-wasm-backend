051afb2da53c4d4293ae028bea73c7f61652ab53
Cody Casterline <cody.casterline@gmail.com>
2025-05-04 12:40:10 -0700

    Clean up the backend lib for publish.

--- crates/ratatui-wasm-backend/src/lib.rs
+++ crates/ratatui-wasm-backend/src/lib.rs
@@ -1,10 +1,14 @@
 //! Provides a WASM-compatible backend for [Ratatui]
 //! 
 //! [Ratatui]: https://ratatui.rs
+//! 
+//! 
 
 pub mod backend;
-pub mod ctrl;
 pub mod types;
+
+#[cfg(feature = "parser")]
+pub mod ctrl;
 
 pub use anes;
 pub use ratatui;
